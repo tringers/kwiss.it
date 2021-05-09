6688d2cee3c6129b9df2b804939ecf450b605229
Timo Riedinger <timoriedinger@posteo.de>
2021-05-09 11:49:16 +0200

    Tests customized. base.html created. Bootstrap 5 downloaded.

--- kwiss_it/tests.py
+++ kwiss_it/tests.py
@@ -12,9 +12,24 @@ class SecretKeyTest(TestCase):
 
 
 
-class CheckWebsite(TestCase):
+class WebsiteTest(TestCase):
 
 	def test_load_start_page(self):
 		client = Client()
 		response = client.get('/')
 		self.assertEqual(200, response.status_code)
+
+	def test_load_hello_leduc(self):
+		client = Client()
+		response = client.get('/Le Duc/')
+		self.assertIn(b"Hello Le Duc!", response.content)
+
+	def test_load_hello_sascha(self):
+		client = Client()
+		response = client.get('/Sascha/')
+		self.assertIn(b"Hello Sascha!", response.content)
+
+	def test_load_hello_timo(self):
+		client = Client()
+		response = client.get('/Timo/')
+		self.assertIn(b"Hello Timo!", response.content)
