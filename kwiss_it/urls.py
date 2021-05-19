9f6556227579c6eb46ad9c5f7e765091aa127dbd
lischetzke <6329950+lischetzke@users.noreply.github.com>
2021-05-19 19:28:25 +0200

    Added datenschutz.html and impressum.html

--- kwiss_it/urls.py
+++ kwiss_it/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
+	path('datenschutz', views.datenschutz, name='datenschutz'),
+	path('impressum', views.impressum, name='impressum'),
 ]
--- kwiss_it/views.py
+++ kwiss_it/views.py
@@ -5,3 +5,11 @@ from django.template import loader
 
 def index(request):
 	return render(request, 'kwiss_it/index.html')
+
+
+def datenschutz(request):
+	return render(request, 'kwiss_it/datenschutz.html')
+
+
+def impressum(request):
+	return render(request, 'kwiss_it/impressum.html')
