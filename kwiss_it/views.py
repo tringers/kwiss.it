1e3abc7c812f337466acd102a3aaa7d6b76680f5
FallenSeraph11111 <Vollmer-Sascha@web.de>
2021-05-26 15:52:35 +0200

    added more views and templates (without content)

--- kwiss_it/views.py
+++ kwiss_it/views.py
@@ -13,3 +13,27 @@ def datenschutz(request):
 
 def impressum(request):
 	return render(request, 'kwiss_it/impressum.html')
+
+
+def lobby(request, lobby_id):
+	return render(request, 'kwiss_it/lobby.html')
+
+
+def settings(request):
+	return render('kwiss_it/settings.html')
+
+
+def register(request):
+	return render('kwiss_it/register.html')
+
+
+def user(request):
+	return render('kwiss_it/user.html')
+
+
+def login(request):
+	return render('kwiss_it/login.html')
+
+
+def review(request):
+	return render('kwiss_it/review.html')
