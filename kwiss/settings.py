c957ec2e335d88484bdf2fca535b8ad941266ac7
lischetzke <6329950+lischetzke@users.noreply.github.com>
2021-05-27 09:17:40 +0200

    Preparation for API, design changed in base.html

--- kwiss/settings.py
+++ kwiss/settings.py
@@ -34,7 +34,10 @@ except FileNotFoundError:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    '10.150.1.6',
+]
 
 
 # Application definition
---
+++ kwiss_it/apiview.py
@@ -0,0 +1,8 @@
+from rest_framework import routers, serializers, viewsets
+
+
+
+
+
+router = routers.DefaultRouter()
+#router.register(r'sensor', SensorViewSet)
