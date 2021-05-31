7e738bb680fa5e4605a36e88d02ed8c344034006
FallenSeraph11111 <Vollmer-Sascha@web.de>
2021-05-31 15:12:23 +0200

    Changes by mori, admin.py implemented

--- kwiss_it/admin.py
+++ kwiss_it/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from kwiss_it.models import Picture, UserDescription, UserPicture, UserLastSeen, Score, State, Category, Score_Category, QuestionType, Question, Answer, ReportType, Report, DiscordRole
 
-# Register your models here.
+admin.site.register(Picture)
+admin.site.register(UserDescription)
+admin.site.register(UserPicture)
+admin.site.register(UserLastSeen)
+admin.site.register(Score)
+admin.site.register(State)
+admin.site.register(Category)
+admin.site.register(Score_Category)
+admin.site.register(QuestionType)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(ReportType)
+admin.site.register(Report)
+admin.site.register(DiscordRole)
