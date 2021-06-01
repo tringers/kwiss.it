69da8bde95a6cd3195cacc236c18338cc309a5f2
Timo Riedinger <timoriedinger@posteo.de>
2021-06-01 17:57:14 +0200

    Added some tests in the tests.py

--- kwiss_it/tests.py
+++ kwiss_it/tests.py
@@ -17,15 +17,49 @@ class SeparateFileTest(TestCase):
 		self.assertTrue(f, message)
 
 
-class WebsiteTest(TestCase):
+class WebsiteLoadTest(TestCase):
 
-	pages = [
-		"/", "datenschutz/", "impressum/",
-		"login/", "logout/", "register/", "password-reset/",
-		"user/", "createlobby/",
-	]
-
-	def test_load_page(self):
+	def test_load_start_page(self):
 		client = Client()
 		response = client.get('/')
+		self.assertEqual(200, response.status_code)
+
+	def test_load_datenschutz_page(self):
+		client = Client()
+		response = client.get('/datenschutz')
+		self.assertEqual(200, response.status_code)
+
+	def test_load_impressum_page(self):
+		client = Client()
+		response = client.get('/impressum')
+		self.assertEqual(200, response.status_code)
+
+	def test_load_login_page(self):
+		client = Client()
+		response = client.get('/login')
+		self.assertEqual(200, response.status_code)
+
+	def test_load_logout_page(self):
+		client = Client()
+		response = client.get('/logout')
+		self.assertEqual(200, response.status_code)
+
+	def test_load_register_page(self):
+		client = Client()
+		response = client.get('/register')
+		self.assertEqual(200, response.status_code)
+
+	def test_load_password_reset_page(self):
+		client = Client()
+		response = client.get('/password-reset')
+		self.assertEqual(200, response.status_code)
+
+	def test_load_user_page(self):
+		client = Client()
+		response = client.get('/user/test')
+		self.assertEqual(200, response.status_code)
+
+	def test_load_createlobby_page(self):
+		client = Client()
+		response = client.get('/createlobby')
 		self.assertEqual(200, response.status_code)
