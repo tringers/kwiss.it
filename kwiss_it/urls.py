0fef0e186cec628b68467842dd509a3ce2d88294
lischetzke <6329950+lischetzke@users.noreply.github.com>
2021-06-02 23:44:39 +0200

    Restructure

--- kwiss_it/urls.py
+++ kwiss_it/urls.py
@@ -16,8 +16,6 @@ urlpatterns = [
 	path('', views.index, name='index'),
 	path('datenschutz', views.datenschutz, name='datenschutz'),
 	path('impressum', views.impressum, name='impressum'),
-	re_path(r'lobby/(?P<lobby_key>[0-9A-F]{6})/$', views.lobby_view, name='lobby'),
-	re_path(r'lobby/(?P<lobby_key>[0-9A-F]{6})/(?P<auth_token>[0-9a-f\-]{0,36})/$', views.lobby_view, name='lobby'),
 
 	path('settings', views.settings, name='settings'),
 	path('review', views.review, name='review'),
@@ -28,13 +26,18 @@ urlpatterns = [
 	path('register/checkusername/', views.register_checkusername_short, name='register_checkusername_short'),
 	path('register/checkusername/<str:username>', views.register_checkusername, name='register_checkusername'),
 	path('password-reset', views.login, name='vergessen'),
+
 	path('u', views.user, name='userShort'),
 	path('u/', views.user, name='userShort'),
 	path('u/<str:username>', views.user_short, name='userShort'),
 	path('user', views.user, name='user'),
 	path('user/', views.user, name='user'),
 	path('user/<str:username>', views.user_profile, name='userProfile'),
+
 	path('createlobby', views.createlobby_view, name='createlobby'),
 	path('lobbylist', views.lobbylist_view,name='lobbylist'),
+	re_path(r'lobby/(?P<lobby_key>[0-9A-F]{6})/$', views.lobby_view, name='lobby'),
+	re_path(r'lobby/(?P<lobby_key>[0-9A-F]{6})/(?P<auth_token>[0-9a-f\-]{0,36})/$', views.lobby_view, name='lobby'),
+
 	path('api/', include(router.urls)),
 ]
