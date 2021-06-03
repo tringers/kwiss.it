ca10220967b741e3d0ccc3716755b5742b98c78a
FallenSeraph11111 <Vollmer-Sascha@web.de>
2021-06-03 14:52:33 +0200

    lobbylist json script moved to seperate file

--- jsmin.bat
+++ jsmin.bat
@@ -3,3 +3,4 @@
 node jsmin.js kwiss_it\static\js\validation.js kwiss_it\static\js\validation.min.js
 node jsmin.js kwiss_it\static\js\usersettings.js kwiss_it\static\js\usersettings.min.js
 node jsmin.js kwiss_it\static\js\createLobby.js kwiss_it\static\js\createLobby.min.js
+node jsmin.js kwiss_it\static\js\lobbylist.js kwiss_it\static\js\lobbylist.min.js
---
+++ kwiss_it/static/js/lobbylist.js
@@ -0,0 +1,65 @@
+ // Get Lobbytypes first
+        list_lobbytype = [];
+
+        fetch('/api/lobbytype/')
+            .then(data => data.json()
+                .then(json => {
+                    for(let i = 0; i < json.length; i++) {
+                        let lobbytype = json[i];
+                        let id = parseInt(lobbytype.LTid);
+                        list_lobbytype[id] = lobbytype.LTdescription;
+                    }
+                    getLobbys();
+                })
+                .catch(e => {
+                    // Error parsing data to json
+                }))
+            .catch(e => {
+                // Error fetching data from api
+            });
+
+        function getLobbys() {
+            fetch('/api/lobby/')
+                .then(data => data.json()
+                    .then(json => {
+                        let table_body = document.getElementById('table-body');
+
+                        for (let i = 0; i < json.length; i++) {
+                            let lobby = json[i];
+                            let row = document.createElement('tr');
+
+                            let lobby_private = document.createElement('td');
+                            let lobby_name = document.createElement('td');
+                            let lobby_player = document.createElement('td');
+                            let lobby_mode = document.createElement('td');
+                            let lobby_join = document.createElement('td');
+                            let lobby_join_button = document.createElement('a');
+                            lobby_join_button.innerHTML = 'Beitreten';
+                            lobby_join_button.href = '/lobby/' + lobby.Lkey + '/';
+                            lobby_join_button.classList.add('btn');
+                            lobby_join_button.classList.add('btn-outline-primary');
+                            lobby_join_button.classList.add('btn-sm');
+                            lobby_join_button.classList.add('col-12');
+                            lobby_join_button.setAttribute('role', 'button');
+
+                            lobby_private.innerHTML = lobby.Lprivate ? '&#128274' : '';
+                            lobby_name.innerHTML = lobby.Lname;
+                            lobby_player.innerHTML = '0 / ' + lobby.Lplayerlimit;
+                            lobby_mode.innerHTML = list_lobbytype[lobby.Ltype] ? list_lobbytype[lobby.Ltype] : lobby.Ltype;
+                            lobby_join.appendChild(lobby_join_button);
+
+                            row.appendChild(lobby_private);
+                            row.appendChild(lobby_name);
+                            row.appendChild(lobby_player);
+                            row.appendChild(lobby_mode);
+                            row.appendChild(lobby_join);
+                            table_body.appendChild(row);
+                        }
+                    })
+                    .catch(e => {
+                        // Error parsing data to json
+                    }))
+                .catch(e => {
+                    // Error fetching data from api
+                });
+        }
