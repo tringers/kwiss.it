20de7403757c7e5e466074c57d6a30c44a78aa3d
lischetzke <6329950+lischetzke@users.noreply.github.com>
2021-06-05 05:01:03 +0200

    Added TODOs and notes

--- kwiss_it/views/game.py
+++ kwiss_it/views/game.py
@@ -1,3 +1,16 @@
 from .helper import *
 
-def
+# TODO: Helper functions and checks
+# 		Run these before processing anything else
+# TODO: Use Lobby.Uid for Solo Lobby!
+
+def setCurrentQuestion():
+	# Check here, if lobby's currentQuestion is up-to-date
+	return
+
+def heartbeatCheck():
+	# Check users last_heartbeat and kick him if necessary
+	return
+
+
+
