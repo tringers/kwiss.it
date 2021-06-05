f0d206c664762a28624e6f661573cdd7bd8d1b53
FallenSeraph11111 <Vollmer-Sascha@web.de>
2021-06-06 00:54:01 +0200

    bullshit

--- kwiss_it/static/js/submitusercontent.js
+++ kwiss_it/static/js/submitusercontent.js
@@ -6,10 +6,11 @@ After changing code in here:
 
 let catselect = document.getElementById("category");
 let divcreatecategory = document.getElementById("createcat");
-
+let questionamount = document.getElementById("questionamount");
 let addquestion = document.getElementById("addquestion");
 let divquestions = document.getElementById("questions");
 let template;
+let answertemplate;
 let questionsset = new Set();
 let maxquestion = 0;
 let questiontypes = JSON;
@@ -24,18 +25,18 @@ function categorylist(url = api_url + "/category/") {
     fetch(url)
         .then(data => data.json()
             .then(json => {
-                let next=json.next;
-                let content = json.content
-                if (!content){
+                let next = json.next;
+                let content = json.results
+                if (!content) {
                     return
                 }
-                for(let i=0; i< content.length;i++){
+                for (let i = 0; i < content.length; i++) {
                     let catoption = document.createElement("option")
                     catoption.value = content[i].Cid;
                     catoption.innerText = content[i].Cname;
                     catselect.appendChild(catoption)
                 }
-                if (next != null){
+                if (next != null) {
                     categorylist(next)
                 }
 
@@ -44,26 +45,137 @@ function categorylist(url = api_url + "/category/") {
 }
 
 function getquestiontypes() {
-
+    url = api_url + "/questiontype/"
+    fetch(url)
+        .then(data => data.json()
+            .then(json => {
+                questiontypes = json
+            })
+        );
 }
 
 function getTemplate() {
     let temp = document.getElementById("template");
     template = temp.innerHTML;
-    temp.innerHTML = "";
+    temp.remove()
+    let temp2 = document.getElementById("templateanswer");
+    answertemplate = temp2.innerHTML;
+    temp2.remove()
 }
+
 
 function faddquestion() {
     maxquestion += 1;
+    questionsset.add(maxquestion)
     let divquestion = document.createElement("div");
     divquestion.id = "q" + maxquestion.toString();
     divquestion.innerHTML = template;
-    divquestion.classList.add("border")
+    divquestion.classList.add("border", "row", "col-lg-8", "mx-auto", "p-3")
     divquestions.appendChild(divquestion)
 
 
-}
+    let questionNumber = document.getElementById("questionnumber");
+    let questionNumberField = document.getElementById("questionnumberfield");
+    let qtype = document.getElementById("qtype");
+    let answeramount = document.getElementById("answeramount");
+    let answeramountfield = document.getElementById("answeramountfield");
+    let questiontext = document.getElementById("questiontext");
+    let answers = document.getElementById("answers");
+    let removequestion = document.getElementById("removequestion");
 
+    questionNumber.innerText = "Frage Nummer " + maxquestion.toString();
+    questionNumber.id = "questionnumber" + maxquestion.toString();
+    questionNumber.name = "questionnumber" + maxquestion.toString();
+
+    questionNumberField.id= "questionnumberfield" + maxquestion.toString();
+    questionNumberField.name= "questionnumberfield";
+    questionNumberField.value= maxquestion.toString();
+
+
+
+    for (let i = 0; i < questiontypes.length; i++) {
+        let option = document.createElement("option");
+        option.value = questiontypes[i].QTname;
+        option.innerText = questiontypes[i].QTdescription
+        qtype.appendChild(option)
+    }
+    qtype.id = "qtype" + maxquestion.toString();
+    qtype.name = "qtype" + maxquestion.toString();
+    qtype.addEventListener("change", function () {
+        if (qtype.value == "single" || qtype.value == "multiple") {
+            answeramount.removeAttribute("hidden")
+            answeramountfield.innerText = answeramount.value;
+        } else {
+            answeramount.setAttribute("hidden", "true");
+            answeramountfield.innerText = "1";
+        }
+        answeramountfield.dispatchEvent(new Event("change"))
+    });
+    answeramount.id = "answeramount" + maxquestion.toString();
+    if (qtype.value == "single" || qtype.value == "multiple") {
+        answeramount.removeAttribute("hidden")
+        answeramountfield.innerText = answeramount.value;
+
+    } else {
+        answeramount.setAttribute("hidden", "true");
+        answeramountfield.innerText = "1";
+    }
+    answeramount.addEventListener("change",function (){
+        answeramountfield.innerText=answeramount.value;
+        answeramountfield.dispatchEvent(new Event("change"))
+    })
+    answeramountfield.addEventListener("change", function () {
+
+        answers.innerHTML = "";
+        let amount = parseInt(answeramountfield.innerText);
+        for (let i = 0; i < amount; i++) {
+            let divanswer = document.createElement("div");
+            divanswer.innerHTML = answertemplate;
+            answers.appendChild(divanswer);
+            divanswer.id = "question" + maxquestion.toString() + "answer" + i.toString();
+            divanswer.classList.add("border", "row", "col-lg-10", "mx-auto", "p-3", "rounded");
+
+            let answernumber = document.getElementById("answernumber");
+            let answernumberfield = document.getElementById("answernumberfield");
+            let answertext = document.getElementById("answertext");
+            let correct = document.getElementById("correct");
+
+            answernumber.innerText = "Antwort " + (i + 1).toString();
+            answernumber.id = "question" + maxquestion.toString() + "answernumber" + i.toString()
+            answernumberfield.id = "question" + maxquestion.toString() + "answernumberfield" + i.toString()
+            answernumberfield.name = "question" + maxquestion.toString() + "answernumberfield" + i.toString()
+            answernumberfield.innerText = i.toString();
+            answertext.id = "question" + maxquestion.toString() + "answertext" + i.toString();
+            answertext.name = "question" + maxquestion.toString() + "answertext" + i.toString();
+            correct.id = "question" + maxquestion.toString() + "correct" + i.toString();
+            if (qtype.value == "multiple") {
+                correct.name = "question" + maxquestion.toString() + "correct" + i.toString();
+            } else if (qtype.value == "number_exact" | qtype.value == "number_deviation") {
+                correct.name = "question" + maxquestion.toString() + "correct";
+                correct.type = "radio";
+                //correct.setAttribute("hidden", "true");
+                answertext.type = "number"
+            } else {
+                correct.name = "question" + maxquestion.toString() + "correct";
+                correct.type = "radio";
+            }
+        }
+
+    })
+    answeramountfield.dispatchEvent(new Event("change"))
+    answeramountfield.id = "answeramountfield" + maxquestion.toString();
+    answeramountfield.name = "answeramountfield" + maxquestion.toString();
+    questiontext.id = "questiontext" + maxquestion.toString();
+    questiontext.name = "questiontext" + maxquestion.toString();
+    removequestion.id = "removequestion" + maxquestion.toString();
+    removequestion.addEventListener("click", function () {
+        questionsset.delete(divquestion.substring(1))
+        questionamount.value=JSON.stringify(questionsset)
+        divquestion.remove()
+    })
+    questionamount.value=JSON.stringify(questionsset)
+
+}
 
 catselect.addEventListener("change", function () {
     if (catselect.value == "new") {
--- kwiss_it/views/content.py
+++ kwiss_it/views/content.py
@@ -6,6 +6,12 @@ def add_content(request,args=None):
 			'errorMsg': '',
 			'infoMsg': ''
 		}
+	if request.method != 'POST':
+		return render(request, 'kwiss_it/submitusercontent.html', args)
+
+	input_question_amount = request.POST.get("answeramountfield1")
+	args['infoMsg']=input_question_amount
 
 
+	print(input_question_amount)
 	return render(request, 'kwiss_it/submitusercontent.html', args)
