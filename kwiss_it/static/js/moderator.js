924b3d1260d558eae5137ac20d065d821665d0e8
FallenSeraph11111 <Vollmer-Sascha@web.de>
2021-06-07 04:15:11 +0200

    moderator

--- kwiss_it/static/js/moderator.js
+++ kwiss_it/static/js/moderator.js
@@ -3,38 +3,87 @@ IMPORTANT:
 After changing code in here:
   Please run jsmin.bat or equivalent.
  */
-let cattemplate="templateaccordionitemclass";
-let qtemplate="templateaccordionitemquestion";
-let atemplate="templateanswer";
+const cattemplate = document.getElementById("templateaccordionitemclass");
+const qtemplate = document.getElementById("templateaccordionitemquestion");
+const atemplate = document.getElementById("templateanswer");
 
-function start(){
+const accordioncat = document.getElementById("accordioncat")
 
-}
-function fetchCategory(pending = false, clear = false) {
-    let fetch_param = '?approved=true' + (pending ? '&pending=true' : '');
-    if (clear) {
-        current_page = 1;
-        categories = {};
-    }
-    // Get max page
-    fetch(api_url + '/category/' + fetch_param + '&page=1')
-        .then(pData => pData.json()
-            .then(pJson => {
-                max_page = Math.ceil(pJson.count / 10);
-                fetch(api_url + '/category/' + fetch_param + '&page=' + current_page)
-                    .then(data => data.json()
-                        .then(json => {
-                            for (let i = 0; i < json.results.length; i++) {
-                                let cat = json.results[i];
-                                if (categories[cat.Cname] === undefined)
-                                    categories[cat.Cname] = cat;
-                            }
-                            refreshCategoryAmount();
-                            refreshTable();
-                        }));
-            }));
+function start() {
+    categorylist()
 }
 
+
+
+
+function categorylist(url = api_url + "/category/?approved=true&pending=true&denied=true") {
+    fetch(url)
+        .then(data => data.json()
+            .then(json => {
+                let next = json.next;
+                let content = json.results
+                if (!content) {
+                    return
+                }
+                for (let i = 0; i < content.length; i++) {
+                    let divcat = document.createElement("div")
+                    divcat.innerHTML=cattemplate.innerHTML;
+                    divcat.classList.add("accordion-item");
+                    accordioncat.appendChild(divcat);
+
+                    let accordionheader = document.getElementById("accordionheader")
+                    let collapseOne = document.getElementById("collapseOne")
+                    accordionheader.id="accordionheader"+i.toString();
+                    accordionheader.setAttribute("data-bs-target","collapseOne"+i.toString());
+                    collapseOne.id="collapseOne"+i.toString();
+
+
+                    let catname = document.getElementById("catname")
+                    let catdesc = document.getElementById("catdesc")
+                    let catstate = document.getElementById("catstate")
+                    let btnpromote = document.getElementById("promote")
+                    let btndemote = document.getElementById("demote")
+
+                    catname.innerText= content[i].Cname;
+                    catname.id="catname"+i.toString();
+                    catdesc.innerText= content[i].Cdescription;
+                    catdesc.id="catdesc"+i.toString();
+                    catstate.innerText= (content[i].STid == 1)? "approved" : (content[i].STid == 2)? "pending" : "denied";
+                    catstate.id="catstate"+i.toString();
+                    btnpromote.value=(content[i].STid == 1)? "pending" : "approved";
+                    btnpromote.id="promote"+i.toString();
+                    btndemote.value=(content[i].STid == 3)? "pending" : "denied" ;
+
+                    btnpromote.addEventListener("click",function (){
+
+                    })
+                    btndemote.addEventListener("click",function (){
+
+                    })
+
+                    let accordionquestions = document.getElementById("accordionquestions")
+                    accordionquestions.id="accordionquestions"+i.toString();
+                    addquestion(i.toString())
+
+                }
+                if (next != null) {
+                    categorylist(next)
+                }
+
+            })
+        );
+}
+
+function addquestion(i) {
+    let accordionquestions = document.getElementById("accordionquestions"+i)
+    let divquestion= document.createElement("div")
+    divquestion.classList.add("accordion-item");
+    divquestion.innerHTML= qtemplate.innerHTML;
+    accordionquestions.appendChild(divquestion)
+
+
+
+}
 
 
 document.addEventListener("DOMContentLoaded", start);
